cd7fc858d09d0a019d5a961c4c8df51ef2e43373
FieryMewtwo <77066742+FieryMewtwo@users.noreply.github.com>
2021-05-10 09:56:18 -0400

    Create mudkip.asm

---
+++ data/pokemon/dex_entries/mudkip.asm
@@ -0,0 +1,10 @@
+	db "MUD FISH@" ; species name
+	dw 104, 100 ; height, weight
+
+	db   "Its large tail fin"
+	next "propels it through"
+	next "water with powerful"
+
+	page "acceleration. It is"
+	next "strong in spite of"
+	next "its size.@"
